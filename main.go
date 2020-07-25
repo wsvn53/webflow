b48b635dca6865c62ad090f85a32bcb5d9bbe449
ethan <wsvn53@gmail.com>
2020-07-25 22:45:33 +0800

    support flow content from argument or stdin

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/cosiner/flag"
 	"io/ioutil"
 	"os"
@@ -8,17 +9,21 @@ import (
 
 type Cli struct {
 	Flowfile	string	`names:"-f, --file" usage:"Specify Flowfile."`
+	FlowContent	string	`names:"-c, --flow" usage:"Raw flow contents."`
 }
 
 func (t *Cli) Metadata() map[string]flag.Flag {
 	return map[string]flag.Flag{
 		"": {
-			Usage:   "webflow with Flowfile",
+			Usage:   "webflow",
 			Version: "v0.0.1",
 			Desc:    "Webflow can perform a series of web operations defined by Flowfile.",
 		},
 		"--file": {
-			Desc: "Specify Flowfile.",
+			Desc: "path of flowfile to run.",
+		},
+		"--flow": {
+			Desc: "using flow raw content string.",
 		},
 	}
 }
@@ -28,9 +33,20 @@ func main() {
 	err := flag.NewFlagSet(flag.Flag{}).ParseStruct(&cli, os.Args...)
 	assertErr("FlagSet", err)
 
-	flowBytes, err := ioutil.ReadFile(cli.Flowfile)
-	assertErr("Flowfile", err)
-	flowContents := string(flowBytes)
+	flowContents := cli.FlowContent
+
+	if len(cli.Flowfile) > 0 {
+		flowBytes, err := ioutil.ReadFile(cli.Flowfile)
+		assertErr("Flowfile", err)
+		flowContents = string(flowBytes)
+	}
+
+	if len(flowContents) == 0 {
+		flowBytes, err := ioutil.ReadAll(os.Stdin)
+		assertErr("Stdin", err)
+		flowContents = string(flowBytes)
+		fmt.Println(flowContents)
+	}
 
 	flow := FlowFromString(flowContents)
 	browser := NewBrowser(flow)
