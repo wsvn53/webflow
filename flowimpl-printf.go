703e13912a6ffcd3f8af4e7bd403d0850c34eeab
ethan <wsvn53@gmail.com>
2020-07-26 00:17:07 +0800

    add printf instruction

---
+++ flowimpl-printf.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"github.com/jinzhu/copier"
+	"reflect"
+	"strings"
+)
+
+type FlowImplPrintf FlowImplBase
+
+func (impl *FlowImplPrintf) Type() FlowImplType {
+	return FlowImplTypeOP
+}
+
+func (impl *FlowImplPrintf) Do(args...interface{}) error {
+	browser := args[0].(*Browser)
+
+	format := impl.command.Fields[0].ToString()
+	var fields []interface{}
+	for _, v := range impl.command.Fields[1:] {
+		vs := v.ToString()
+		if _, ok := browser.variableMaps[vs]; ok && strings.HasPrefix(vs, "$") {
+			fields = append(fields, browser.variableMaps[vs])
+			continue
+		}
+		fields = append(fields, vs)
+	}
+
+	if len(fields) == 0 {
+		return nil
+	}
+
+	fmt.Printf(format, fields...)
+	return nil
+}
+
+//go:generate make IMPL_TYPE=FlowImplPrintf gen-impl
+
+func init() {
+	flowImpl := func() IFlowImpl {
+		return &FlowImplPrintf{}
+	}()
+	registerFlow(flowImpl)
+}
+
+func (impl *FlowImplPrintf) Name() string {
+	interfaceName := reflect.TypeOf(impl).String()
+	commandName := strings.Split(interfaceName, "FlowImpl")[1]
+	return strings.ToLower(commandName)
+}
+
+func (impl *FlowImplPrintf) SetCommand(command *FlowCommand) {
+	impl.command = command
+}
+
+func (impl *FlowImplPrintf) Command() *FlowCommand {
+	return impl.command
+}
+
+func (impl *FlowImplPrintf) Clone() IFlowImpl {
+	c := &FlowImplPrintf{}
+	_ = copier.Copy(c, impl)
+	return c
+}
