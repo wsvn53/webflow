de4215030b5d538e51b9de9eb02f34eb60621a6c
ethan <wsvn53@gmail.com>
2020-07-25 22:48:46 +0800

    support print version command

--- main.go
+++ main.go
@@ -7,16 +7,19 @@ import (
 	"os"
 )
 
+const VERSION = "v0.0.1"
+
 type Cli struct {
-	Flowfile	string	`names:"-f, --file" usage:"Specify Flowfile."`
-	FlowContent	string	`names:"-c, --flow" usage:"Raw flow contents."`
+	Flowfile	string	`names:"-f, --file" usage:"Specify Flowfile path."`
+	FlowContent	string	`names:"-c, --flow" usage:"Using raw flow content string."`
+	PrintVersion bool	`names:"-v, --version" usage:"Print webflow version."`
 }
 
 func (t *Cli) Metadata() map[string]flag.Flag {
 	return map[string]flag.Flag{
 		"": {
 			Usage:   "webflow",
-			Version: "v0.0.1",
+			Version: VERSION,
 			Desc:    "Webflow can perform a series of web operations defined by Flowfile.",
 		},
 		"--file": {
@@ -32,6 +35,11 @@ func main() {
 	var cli Cli
 	err := flag.NewFlagSet(flag.Flag{}).ParseStruct(&cli, os.Args...)
 	assertErr("FlagSet", err)
+
+	if cli.PrintVersion {
+		fmt.Println(os.Args[0], VERSION)
+		os.Exit(0)
+	}
 
 	flowContents := cli.FlowContent
 
