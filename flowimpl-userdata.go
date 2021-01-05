c41f15cff300a48e2d458ca675f518f5f9851bc3
ethan <wsvn53@gmail.com>
2021-01-05 13:54:17 +0800

    Feature: support specify userdata flag

---
+++ flowimpl-userdata.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"github.com/chromedp/chromedp"
+	"github.com/jinzhu/copier"
+	"reflect"
+	"strings"
+)
+
+type FlowImplUserData FlowImplBase
+
+func (impl *FlowImplUserData) Type() FlowImplType {
+	return FlowImplTypeFlag
+}
+
+func (impl *FlowImplUserData) Usage() string {
+	return impl.Name() + " <string> 	Setup browser userdata storage path."
+}
+
+func (impl *FlowImplUserData) Do(args...interface{}) error {
+	if len(args) == 0 {
+		return nil
+	}
+	opt := args[0].(*chromedp.ExecAllocatorOption)
+	*opt = chromedp.UserAgent(impl.command.GetFieldString(0))
+	return nil
+}
+
+//go:generate make IMPL_TYPE=FlowImplUserData gen-impl
+
+func init() {
+	flowImpl := func() IFlowImpl {
+		return &FlowImplUserData{}
+	}()
+	registerFlow(flowImpl)
+}
+
+func (impl *FlowImplUserData) Name() string {
+	interfaceName := reflect.TypeOf(impl).String()
+	commandName := strings.Split(interfaceName, "FlowImpl")[1]
+	return strings.ToLower(commandName)
+}
+
+func (impl *FlowImplUserData) SetCommand(command *FlowCommand) {
+	impl.command = command
+}
+
+func (impl *FlowImplUserData) Command() *FlowCommand {
+	return impl.command
+}
+
+func (impl *FlowImplUserData) Clone() IFlowImpl {
+	c := &FlowImplUserData{}
+	_ = copier.Copy(c, impl)
+	return c
+}
