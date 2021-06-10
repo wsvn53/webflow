283991866ba69d28666ce5910dc7008cdb91905e
ethan <wsvn53@gmail.com>
2021-06-10 14:20:26 +0800

    release v0.4.2

--- webflow.rb
+++ webflow.rb
@@ -3,9 +3,9 @@
 class Webflow < Formula
   desc "Webflow: web automation tool based on chromedp"
   homepage "https://git.wsen.me/utils/webflow"
-  url "https://git.wsen.me/attachments/ca09eae3-8718-4d9f-a36b-b3698abbc257"
-  version "v0.4.1"
-  sha256 "6a13d2e058b28eb1b687ee261987845cfc88da3cc1f1cdd74401a47f4256efac"
+  url "https://git.wsen.me/attachments/7770809e-2711-4467-bc69-32f631dbd2dd"
+  version "v0.4.2"
+  sha256 "d40a58e42e7fa6e1910329b726902802da91e57fadc38cc1fe6477884d2a0a50"
 
   def install
     system "chmod", "+x", "webflow"
