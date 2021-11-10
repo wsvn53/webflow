1ae8337171eeb33ce10f347a43a10e19d2059a77
ethan <wsvn53@gmail.com>
2021-11-10 19:58:11 +0800

    release webflow homebrew formula

--- webflow.rb
+++ webflow.rb
@@ -3,9 +3,9 @@
 class Webflow < Formula
   desc "Webflow: web automation tool based on chromedp"
   homepage "https://git.wsen.me/utils/webflow"
-  url"https://git.wsen.me/attachments/92c91255-b6da-4d34-b752-68654b19eaec"
-  version"v0.4.7"
-  sha256"2c3c9cf2bdfafeeb5afc6f9e08993030738cffa175160fc51a34a654fe1704eb"
+  url "https://github.com/wsvn53/webflow/releases/download/v0.5.0/webflow-darwin-amd64"
+  version"v0.5.0"
+  sha256"d036a6c42711a667ec5d383083a1ff02c6d07ac91e7bb132d660411518779f49"
 
   def install
     system "chmod", "+x", "webflow"
